91b7bd85482af3a302690143636d8687c5c7d96c
stuart_zhang <stuartpage@qq.com>
2023-08-10 17:02:50 +0800

    至少能闪烁了。

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// https://stackoverflow.com/q/68313104/7202630
+use ::std::{ffi::OsStr, iter, os::windows::ffi::OsStrExt};
+use ::winapi::{_core::ptr::null_mut, shared::{minwindef::DWORD, windef::HWND}, um::winuser::{FindWindowW, FlashWindow}};
 fn main() {
-    println!("Hello, world!");
+    let winname: Vec<u16> = win32_string("敏行");
+    let hwnd: HWND = unsafe {
+        FindWindowW(null_mut(),winname.as_ptr())
+    };
+    if !hwnd.is_null() {
+        println!("HWND: {:?}", hwnd);
+        unsafe {
+            FlashWindow(hwnd, 0)
+        };
+    }
+}
+fn win32_string(value: &str) -> Vec<u16> {
+    OsStr::new(value).encode_wide().chain(iter::once(0)).collect()
 }
