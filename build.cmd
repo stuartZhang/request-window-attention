97144cfc91415f395d12b9576495a01b895c61bf
stuart_zhang <stuartpage@qq.com>
2023-08-17 23:36:00 +0800

    借助编译器的环境变量 RUSTUP_TOOLCHAIN 与 CARGO_TARGET_DIR，实现从同一代码分支，同时交叉编译输出 x64 与 ia32 两类结果。

--- build.cmd
+++ build.cmd
@@ -2,12 +2,18 @@
 @REM %~1 = nodejs | nw | run | clean
 @REM RWA_NODE_NW_VERSION = v0.49.2 | v16.4.0
 @REM RWA_WIN_ARCH = win-ia32 | win-x64 | win-arm64
+if "%RWA_WIN_ARCH%" == "win-ia32" (
+    set RUSTUP_TOOLCHAIN=nightly-i686-pc-windows-msvc
+    set CARGO_TARGET_DIR=target-%RWA_WIN_ARCH%
+)
 setlocal
 set RWA_PKG_NAME=request-window-attention
-set RWA_DIST_DIR=.\dist\%~1\%RWA_NODE_NW_VERSION%\%RWA_WIN_ARCH%
+set RWA_DIST_DIR=".\dist\%~1\%RWA_NODE_NW_VERSION%\%RWA_WIN_ARCH%"
 if "%~1"=="run" (
     cargo run --package "%~3" --bin "%~5"
 ) else if "%~1"=="clean" (
+    cargo clean
+    set CARGO_TARGET_DIR=target-win-ia32
     cargo clean
     if exist .\dist (
         rmdir /s /q .\dist
--- build4publish.cmd
+++ build4publish.cmd
@@ -1,15 +1,13 @@
 @echo off
 
-set RWA_WIN_ARCH=win-x64
-
-set RWA_NODE_NW_VERSION=v0.49.2
-cmd /C build nw --release
-
 set RWA_NODE_NW_VERSION=v0.54.1
+set RWA_WIN_ARCH=win-x64
 cmd /C build nw --release
-
-set RWA_NODE_NW_VERSION=v0.72.0
+set RWA_WIN_ARCH=win-ia32
 cmd /C build nw --release
 
 set RWA_NODE_NW_VERSION=v16.4.0
+set RWA_WIN_ARCH=win-x64
+cmd /C build nodejs --release
+set RWA_WIN_ARCH=win-ia32
 cmd /C build nodejs --release
